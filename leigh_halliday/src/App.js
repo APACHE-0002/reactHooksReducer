c995f52addf6253ababaedb53f32a9b4820a30a6
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-09-18 23:47:05 -0300

    comitbase34

--- leigh_halliday/src/App.js
+++ leigh_halliday/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import './styles.css'
+import React from "react";
+import "./App.css";
 
+import WithReducer from "./WithReducer";
+import WithContext from "./WithContext";
+import WithIntersection from "./WithIntersection";
+import StateMachineIntro from "./StateMachineIntro";
+import WithStateMachine from "./WithStateMachine";
 
-export default function App() {
-  
-
+function App() {
   return (
-    <>
-      <h1>churrasco</h1>
-    </>
+    <div className="App">
+      <h1>Data Loading Examples</h1>
+      <p>
+        These examples cover the use of useReducer, useContext,
+        IntersectionObserver, and managing the loading state using XState.
+      </p>
 
+      <h2>useReducer</h2>
+      <WithReducer />
+
+      <h2>useContext</h2>
+      <WithContext />
+
+      <h2>IntersectionObserver</h2>
+      <WithIntersection />
+
+      <h2>XState Intro</h2>
+      <StateMachineIntro />
+
+      <h2>XState Data Loading Service</h2>
+      <WithStateMachine />
+    </div>
   );
 }
+
+export default App;
